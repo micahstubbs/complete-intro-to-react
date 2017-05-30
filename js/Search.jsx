adeda40a53c45483f7caa5de542b8b4f38220413
Micah Stubbs <micah.stubbs@gmail.com>
2017-05-30 13:28:43 -0700

    render show posters on search page

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,7 +3,21 @@ import preload from '../data.json';
 
 const Search = () => (
   <div className="search">
-    <pre>{JSON.stringify(preload, null, 4)}</pre>
+    <div>
+      {preload.shows.map(show => (
+        <div className="show-card">
+          <img
+            alt={`${show.title} Show Poster`}
+            src={`/public/img/posters/${show.poster}`}
+          />
+          <div>
+            <h3>{show.title}</h3>
+            <h4>({show.year})</h4>
+            <p>{show.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
   </div>
 );
 
