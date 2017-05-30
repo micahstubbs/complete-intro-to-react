cd438e3a4c25bc72977042e627b7dca7ccb5a915
Micah Stubbs <micah.stubbs@gmail.com>
2017-05-30 09:44:36 -0700

    props example

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,22 +1,25 @@
 /* global React ReactDOM */
 
-var MyTitle = function() {
-  // prettier-ignore
-  return React.createElement('div', null,
-    React.createElement('h1', null, 'Check out this component!')
+var ce = React.createElement;
+
+// prettier-ignore
+var MyTitle = function (props) {
+  return (
+    ce('div', null,
+      ce('h1', null, props.title)
+    )
   );
 };
 
 // prettier-ignore
-var MyFirstComponent = function() {
-  return React.createElement('div', null,
-    React.createElement(MyTitle, null),
-    React.createElement(MyTitle, null),
-    React.createElement(MyTitle, null)
+var MyFirstComponent = function () {
+  return (
+    ce('div', null,
+      ce(MyTitle, {title: 'House of Cards'}),
+      ce(MyTitle, {title: 'Orange is the New Black'}),
+      ce(MyTitle, {title: 'Stranger Things'})
+    )
   );
 };
 
-ReactDOM.render(
-  React.createElement(MyFirstComponent),
-  document.getElementById('app')
-);
+ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
