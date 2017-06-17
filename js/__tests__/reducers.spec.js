7d2489a44ac7949caa9df6a3f9218b06e0b126a8
Micah Stubbs <micah.stubbs@gmail.com>
2017-06-17 12:00:35 -0700

    add reducers test generated by redux devtools

---
+++ js/__tests__/reducers.spec.js
@@ -0,0 +1,9 @@
+import reducers from '../reducers';
+
+test('reducers', () => {
+  const state = reducers(
+    { searchTerm: '', apiData: {} },
+    { type: 'SET_SEARCH_TERM', payload: 'black' }
+  );
+  expect(state).toEqual({ searchTerm: 'black', apiData: {} });
+});
