5bbb7aecf47b54f8d05493c4abcd581718efb43f
Micah Stubbs <micah.stubbs@gmail.com>
2017-06-17 13:00:21 -0700

    add test case for ADD_API_DATA

--- js/__tests__/reducers.spec.js
+++ js/__tests__/reducers.spec.js
@@ -7,3 +7,35 @@ test('SET_SEARCH_TERM', () => {
   );
   expect(state).toEqual({ searchTerm: 'black', apiData: {} });
 });
+
+test('ADD_API_DATA', () => {
+  const state = reducers(
+    { searchTerm: 'west', apiData: {} },
+    {
+      type: 'ADD_API_DATA',
+      payload: {
+        rating: '6.3',
+        title: 'Westworld',
+        year: '2016–',
+        description: 'Set at the intersection of the near future and the reimagined past, explore a world in which every human appetite, no matter how noble or depraved, can be indulged without consequence.',
+        poster: 'ww.jpg',
+        imdbID: 'tt0475784',
+        trailer: 'eX3u0IlBBO4'
+      }
+    }
+  );
+  expect(state).toEqual({
+    searchTerm: 'west',
+    apiData: {
+      tt0475784: {
+        rating: '6.3',
+        title: 'Westworld',
+        year: '2016–',
+        description: 'Set at the intersection of the near future and the reimagined past, explore a world in which every human appetite, no matter how noble or depraved, can be indulged without consequence.',
+        poster: 'ww.jpg',
+        imdbID: 'tt0475784',
+        trailer: 'eX3u0IlBBO4'
+      }
+    }
+  });
+});
