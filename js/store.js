e77012620e34678e02f96de863662d47ab9e2449
Micah Stubbs <micah.stubbs@gmail.com>
2017-06-17 11:14:04 -0700

    add async redux so that ratings data is cached in the redux state once it's loaded the first time

--- js/store.js
+++ js/store.js
@@ -1,11 +1,15 @@
 // @flow
 
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import reducer from './reducers';
 
 const store = createStore(
   reducer,
   compose(
+    applyMiddleware(thunk),
+    // middleware that intercepts all the actions
+    // and feeds them to the devtools
     typeof window === 'object' &&
       typeof window.devToolsExtension !== 'undefined'
       ? window.devToolsExtension()
